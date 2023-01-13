0da0f35b7b20beb2af635b53e62a4520f1abda6f
Josh Pierce <joshua.alan.pierce@gmail.com>
2023-01-12 21:38:11 -0700

    Add teams display from fetch call

--- src/Components/ViewTeams/ViewTeams.js
+++ src/Components/ViewTeams/ViewTeams.js
@@ -1,22 +1,39 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import './ViewTeams.css'
 import Header from '../Header/Header'
+import { Link } from 'react-router-dom'
+// import fetchApi from '../../fetchApi'
+
+const url = "https://api-nba-v1.p.rapidapi.com/teams"
 
 const ViewTeams = () => {
-  // const [allTeams, setTeams] = useState([{}])
-  const allTeams = [{
-      id: 1, name: "Atlanta Hawks", logo: "https://upload.wikimedia.org/wikipedia/fr/e/ee/Hawks_2016.png"},
-    {
-      id: 2, name: "Boston Celtics", logo: "https://upload.wikimedia.org/wikipedia/fr/thumb/6/65/Celtics_de_Boston_logo.svg/1024px-Celtics_de_Boston_logo.svg.png"
-    }
-  ]
+  const [allTeams, setTeams] = useState([{}])
+
+  useEffect(() => {
+    fetch("https://api-nba-v1.p.rapidapi.com/teams", {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '9f4427e2c7mshfdc54bf392d3f70p104469jsnb7e0ca455b92',
+        'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
+        }
+      })
+      .then(response => response.json())
+      .then(data => {
+        const nbaTeams = data.response.filter(team => {
+          return team.nbaFranchise === true && team.name !== "Home Team Stephen A"
+        })
+        setTeams(nbaTeams.sort())
+      })
+  }, [])
 
   const teamCard = allTeams.map(team => {
     return (
+      <Link to="/roster">
       <div className='team-card'>
         <img className='team-image' src={team.logo} alt={"Image of " + team.name}></img>
         <p>{team.name}</p>
       </div>
+      </Link>
     )
   })
 
