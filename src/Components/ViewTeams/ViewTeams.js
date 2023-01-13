ade297f127915ba34d9d0e133cae36bfaa38ecbf
Josh Pierce <joshua.alan.pierce@gmail.com>
2023-01-12 21:02:33 -0700

    Add hard coded data to display to team cards

--- src/Components/ViewTeams/ViewTeams.js
+++ src/Components/ViewTeams/ViewTeams.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import {useState} from 'react'
 import './ViewTeams.css'
 import Header from '../Header/Header'
 
 const ViewTeams = () => {
+  // const [allTeams, setTeams] = useState([{}])
+  const allTeams = [{
+      id: 1, name: "Atlanta Hawks", logo: "https://upload.wikimedia.org/wikipedia/fr/e/ee/Hawks_2016.png"},
+    {
+      id: 2, name: "Boston Celtics", logo: "https://upload.wikimedia.org/wikipedia/fr/thumb/6/65/Celtics_de_Boston_logo.svg/1024px-Celtics_de_Boston_logo.svg.png"
+    }
+  ]
+
+  const teamCard = allTeams.map(team => {
+    return (
+      <div className='team-card'>
+        <img className='team-image' src={team.logo} alt={"Image of " + team.name}></img>
+        <p>{team.name}</p>
+      </div>
+    )
+  })
+
   return (
     <div>
       <Header />
-      <h1>all teams</h1>
+      <div className='team-container'>
+        {teamCard}
+      </div>
     </div>
   )
 }
