29f525c76d85111e1893f37a9c4deb099ddc993a
Josh Pierce <joshua.alan.pierce@gmail.com>
2023-01-15 11:39:42 -0700

    Add images and font style to home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,8 +4,22 @@ import './Home.css'
 
 const Home = () => {
   return (
-    <div>
-      <Header />
+    <div className='home-section'>
+      <header>
+        <h1 className='home-header'>Hooper Hobby</h1>
+      </header>
+      <section className='main-display'>
+        <section className='left-side'>
+          <div className='teams-link'>
+            <h1 className='browse-text'>Browse Teams</h1>
+          </div>
+        </section>
+        <section className='right-side'>
+          <div className='saved-link'>
+            <h1 className='saved-text'>Saved Cards</h1>
+          </div>
+        </section>
+      </section>
     </div>
   )
 }
