cf0a48cb75c34ed28358b34bff1cbd9986f9aa3f
Josh Pierce <joshua.alan.pierce@gmail.com>
2023-01-15 13:41:03 -0700

    Fix bug that was causing styling issues on the home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -9,16 +9,16 @@ const Home = () => {
         <h1 className='home-header'>Hooper Hobby</h1>
       </header>
       <section className='main-display'>
-        <section className='left-side' to="/teams">
+        <div className='left-side' to="/teams">
           <Link to="/teams" className='teams-link' tabIndex={0}>
             <h1 className='browse-text'>Browse Teams</h1>
           </Link>
-        </section>
-        <section className='right-side' to="/my-cards">
+        </div>
+        <div className='right-side' to="/my-cards">
           <Link to="/my-cards" className='saved-link' tabIndex={0}>
             <h1 className='saved-text'>Saved Cards</h1>
           </Link>
-        </section>
+        </div>
       </section>
     </div>
   )
--- src/Components/MyCards/MyCards.js
+++ src/Components/MyCards/MyCards.js
@@ -6,7 +6,7 @@ const MyCards = ({ myCards, deleteCard }) => {
   return (
     <section className='user-cards'>
       <Header />
-      <div className='main-display'>
+      <div className='main-cards-display'>
         <header className='page-header'>
           <h1 className='my-header'>My Cards</h1>
           {myCards.length === 0 && <h3 className='no-cards'>No cards to display, go save some basketball cards!</h3>}
