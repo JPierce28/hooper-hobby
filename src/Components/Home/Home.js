a013b9f14e79fef1edbed9922678c07a69226798
Josh Pierce <joshua.alan.pierce@gmail.com>
2023-01-15 11:49:45 -0700

    remove the underline on home page for links

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from '../Header/Header';
+import { Link } from 'react-router-dom'
 import './Home.css'
 
 const Home = () => {
@@ -9,15 +9,15 @@ const Home = () => {
         <h1 className='home-header'>Hooper Hobby</h1>
       </header>
       <section className='main-display'>
-        <section className='left-side'>
-          <div className='teams-link'>
+        <section className='left-side' to="/teams">
+          <Link to="/teams" className='teams-link' tabIndex={0}>
             <h1 className='browse-text'>Browse Teams</h1>
-          </div>
+          </Link>
         </section>
-        <section className='right-side'>
-          <div className='saved-link'>
+        <section className='right-side' to="/my-cards">
+          <Link to="/my-cards" className='saved-link' tabIndex={0}>
             <h1 className='saved-text'>Saved Cards</h1>
-          </div>
+          </Link>
         </section>
       </section>
     </div>
