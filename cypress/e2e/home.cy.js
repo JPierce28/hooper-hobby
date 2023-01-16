00c5be59d998c7fb8acc095c8d365040448922f7
Josh Pierce <joshua.alan.pierce@gmail.com>
2023-01-15 18:07:10 -0700

    Add tests for the home tests to also test the header functionality

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -22,4 +22,15 @@ describe('Home Page', () => {
     cy.url("http://localhost:3000/my-cards")
     cy.contains("No cards to display, go save some basketball cards!")
   })
+  it("Should not display the teams button in the header if you are on the Teams page", () => {
+    cy.get(".teams-link").click()
+    cy.get(".view-teams-btn").should("not.exist")
+  })
+  it("Should not display the My Cards button in the header if you are on the My Cards page", () => {
+    cy.get(".saved-link").click()
+    cy.get(".my-cards-btn").should("not.exist")
+    cy.get(".view-teams-btn").click()
+    cy.get(".my-cards-btn").click()
+    cy.get(".title").click()
+  })
 })
